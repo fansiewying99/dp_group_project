4bddec0fcd618aae456112523460ea831797f4aa
esh3rwey <omareelsharaawy@gmail.com>
2022-01-21 00:35:56 +0800

    make the checkBounds() method protected

--- VillagersStory/core/src/com/villagersstory/game/GameObjects/NPC.java
+++ VillagersStory/core/src/com/villagersstory/game/GameObjects/NPC.java
@@ -7,7 +7,7 @@ public abstract class NPC extends GameObject{
     protected Random rand = new Random();
     protected int direction = rand.nextInt(4); //false = x
 
-    public void checkBounds(){ //world bounds 1280x540
+    protected void checkBounds(){ //world bounds 1280x540
         if(locationX<0)
             direction = 0;
         else if(locationX>1280)
